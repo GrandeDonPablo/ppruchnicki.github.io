97cecd7a9285e3c1e8cdc5b2a06a533a34393520
GrandeDonPablo <pawelek.poland@gmail.com>
2020-06-16 21:24:28 +0200

    remove nav from mobile version, readjusted header and responsive header

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,8 +45,8 @@
 		// Title Bar.
 			$(
 				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
+					'<span class="title">' + $('#logo').html() +
+					 $('#links').html() + '</span>' +
 				'</div>'
 			)
 				.appendTo($body);
